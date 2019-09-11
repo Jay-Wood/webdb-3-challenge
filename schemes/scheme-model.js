1bb96e0632c27371ef47a9cd4acc841ea110a096
Jay Wood <47437698+jaywood8@users.noreply.github.com>
2019-09-11 16:17:42 -0700

    For airtable

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,10 +1,25 @@
 const db = require("../data/dbConfig.js");
 
 module.exports = {
-    find  
+    find, 
+    findById,
+    add 
 }
 
 function find() {
     return db("schemes");
 }
 
+function findById(id) {
+    return db("schemes")
+        .where({ id })
+        .then(scheme => {
+            res.status(200).json(scheme)
+        })
+}
+
+function add(scheme) {
+    return db("schemes")
+        .insert(scheme)
+        .then(ids => findById(ids[0]))
+}
